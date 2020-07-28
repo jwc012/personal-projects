639b90b649635f16a7da3492b54f142b6f1aac64
Ji Chung <JiChung857@gmail.com>
2020-07-27 22:03:19 -0700

    added a php file to send messages to my email

---
+++ ji-chung/contact-form.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $emailFrom = $_POST['email'];
+  $message = $_POST['message'];
+
+  $emailTo = "jwchungp@gmail.com";
+  $headers = "From: ".$emailFrom;
+  $email_subject = "Personal Website Submission"
+  $email_body = "You have received an e-mail from ".$name.".\n\n".$message;
+
+  mail($emailTo, $email_subject, $email_body, $headers);
+  header("Location: success.html");
+
+}
